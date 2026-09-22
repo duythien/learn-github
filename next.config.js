5b2f6d42b3b9a57a2bab46583688179ef1797f5a
Thien Tran <fcduythien@gmail.com>
2026-09-22 15:21:14 +0700

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+})
+
+module.exports = withPWA({
+	reactStrictMode: true,
+})
+
+
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: 'dist',
+}
+
+module.exports = nextConfig
